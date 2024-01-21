17a43f251af9468baed1020e4b52c5db9a175d1d
mdaijaj <aijaj18@navgurukul.org>
2024-01-21 08:34:32 +0530

    add file

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname)); 
-app.use(cors());
 app.use(cookieParser());
+
+app.use(cors({
+    origin: [""],
+    method: ["POST", "GET"],
+    credentials: true
+}));
 
 
 app.get("/test", (req, res)=>{
